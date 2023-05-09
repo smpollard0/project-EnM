4383aaaa5609abb9a7f5e5c8c38ba8d364f9cc39
Spencer M. Pollard <112116541+smpollard0@users.noreply.github.com>
2023-05-09 09:53:37 -0700

    add all project files for safekeeping

---
+++ EnM_Project/src/main/java/com/mycompany/enm_project/EnM_Project.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.enm_project;
+
+import learn.EField;
+
+/**
+ *
+ * @author spencer
+ */
+public class EnM_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        EField.main(args);
+
+        
+    }
+}
